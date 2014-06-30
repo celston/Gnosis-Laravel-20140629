356d05f35580fe6aedb3b636211e4c21836eba23
celston <celston@gmail.com>
2014-06-29 21:32:32 -0400

    IDE helper

--- app/config/app.php
+++ app/config/app.php
@@ -122,6 +122,9 @@ return array(
 		'Illuminate\View\ViewServiceProvider',
 		'Illuminate\Workbench\WorkbenchServiceProvider',
 
+        'Barryvdh\LaravelIdeHelper\IdeHelperServiceProvider',
+
+        'Gnosis\Lastfm\LastfmServiceProvider'
 	),
 
 	/*
